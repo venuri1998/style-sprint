95bb4381930fdeb35ccc7fe4c27c25d2d705d9b0
venuri1998 <venurihettiarachchi@gmail.com>
2024-04-29 17:51:19 +0530

    api updates

--- src/services/api/stocks.js
+++ src/services/api/stocks.js
@@ -5,7 +5,7 @@ const baseUrl = window?.configs?.apiUrl ? window.configs.apiUrl : "/";
 console.log(baseUrl, 'this is the base url---')
 
 export const getStocks = async () => {
-  const response = await axiosInstance.patch(
+  const response = await axiosInstance.get(
     `${baseUrl}/stocks`
   );
   console.log('this is the response', response, response?.data)
